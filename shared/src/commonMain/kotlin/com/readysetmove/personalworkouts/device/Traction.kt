f178a399398796b18ca051cdadfa2303a11a2923
Robert Pazurek <robert@holistic-dev.com>
2022-09-16 11:07:01 +0200

    feat(traction): add median utility

--- shared/src/commonMain/kotlin/com/readysetmove/personalworkouts/device/Traction.kt
+++ shared/src/commonMain/kotlin/com/readysetmove/personalworkouts/device/Traction.kt
@@ -1,3 +1,17 @@
 package com.readysetmove.personalworkouts.device
 
 data class Traction(val timestamp: Long, val value: Float)
+
+fun List<Traction>.getMedianTraction(): Float {
+    return sortedBy {
+        it.value
+    }.let {
+        if (it.size % 2 == 0) {
+            val left = it[it.size/2]
+            val right = it[it.size/2 + 1]
+            (left.value + right.value)/2
+        } else {
+            it[(it.size/2) + 1].value
+        }
+    }
+}
